03ec4b0c2df7c1edc93bff398aa6535619c61b53
Olav Markussen <olavbm@samfundet.no>
2016-04-11 19:58:40 +0200

    Add uniform initialization of weights.

--- ea_gtype.py
+++ ea_gtype.py
@@ -9,4 +9,6 @@ def random_genotype():
     """
     dims = (hidden_count + output_count
            ,input_count + hidden_count + output_count)
-    return np.random.standard_normal(dims)
+    #return np.zeros(dims)
+    #return np.random.normal(0.4, 0.3, dims)
+    return np.random.uniform(-0.4, 0.4, dims)
